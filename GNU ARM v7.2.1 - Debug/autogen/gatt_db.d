8026513654ec8783625d60373af6c9b8302b5a23
a-shiryaev-arrow <andrey.shiryaev@arrow.com>
2021-04-09 17:17:29 +0300

    initial commit

--- GNU ARM v7.2.1 - Debug/autogen/gatt_db.d
+++
@@ -1,7 +0,0 @@
-autogen/gatt_db.o: ../autogen/gatt_db.c \
- d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\protocol\bluetooth\inc\bg_gattdb_def.h
-
-d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-
-d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\protocol\bluetooth\inc\bg_gattdb_def.h:
--- GNU ARM v7.2.1 - Debug/gecko_sdk_3.1.1/platform/service/system/src/sl_system_init.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_3.1.1/platform/service/system/src/sl_system_init.o: \
- D:/IDEs/SiLabs/Simplicity\ v5/developer/sdks/gecko_sdk_suite/v3.1/platform/service/system/src/sl_system_init.c \
- d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- D:\SW\SiLabs\soc_btmesh_light\autogen/sl_event_handler.h
-
-d:\ides\silabs\simplicity\ v5\developer\sdks\gecko_sdk_suite\v3.1\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-
-D:\SW\SiLabs\soc_btmesh_light\autogen/sl_event_handler.h:
